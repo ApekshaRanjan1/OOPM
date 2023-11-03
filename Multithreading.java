4a6a338ea9673da19bbd05e3bf29c7d1fe3faf55
ApekshaRanjan1 <115869264+ApekshaRanjan1@users.noreply.github.com>
2023-11-03 23:36:41 +0530

    Add files via upload

---
+++ Multithreading.java
@@ -0,0 +1,30 @@
+//Gunjan Patil 22AD1054
+public class Multithreading {
+    public static void main(String[] args) {
+        
+        Runnable numbersRunnable = new PrintNumbers();
+        Runnable squaresRunnable = new PrintSquares();
+
+        Thread t1 = new Thread(numbersRunnable);
+        Thread t2 = new Thread(squaresRunnable);
+
+        t1.start();
+        t2.start();
+    }
+}
+class PrintNumbers implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
+
+class PrintSquares implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Square: " + i * i);
+        }
+    }
+}
+
---
+++ OddNumbException.java
@@ -0,0 +1,38 @@
+//Gunjan Patil 22AD1054
+import java.util.*;
+public class OddNumbException {
+
+    static int getInputNumber() throws OddException {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number: ");
+            int numb = sc.nextInt();
+            sc.close();
+            if(numb%2!=0)
+            {
+                throw new OddException("Input must be an even number");
+            }
+            return numb;
+    }
+    
+    static int calculateSquare(int numb) {
+        return numb*numb;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int number = getInputNumber();
+            int result = calculateSquare(number);
+            System.out.println("Square ==> "+result);
+        } catch (OddException e) {
+            System.out.println("Error! "+e.getMessage());
+        }
+    }
+}
+class OddException extends Exception {
+    OddException(String message)
+    {
+        super(message);
+    }
+}
+
+
